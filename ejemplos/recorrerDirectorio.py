62c0beb0f59c4f617bd62201ea8aef39b64408e0
Guillermo. Bello. Cameno <gbelloca@usersad.everis.int>
2016-08-16 21:23:18 +0200

    Prueba para recorrer directorios

---
+++ ejemplos/recorrerDirectorio.py
@@ -0,0 +1,9 @@
+import os
+ 
+# Setteamos el directorio raiz a la variable rootDir
+# En realidad la variable se puede llamar como sea :)
+ 
+rootDir = '/home/pi/fotomaton/imagenes/raw'
+for dirName, subdirList, fileList in os.walk(rootDir):
+    print('Directorio encontrado: %s' % dirName)
+    for fname in fileList:
